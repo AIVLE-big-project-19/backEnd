b0a90d6ccd753eac8b52f3d5389fdbcc33ba3b95
s2ungyeon-h <s2ungyeon.h@gamil.com>
2026-07-14 07:13:02 +0900

    feat: RefreshToken 엔티티/Repository, 토큰 해시 유틸 추가

---
+++ src/main/java/com/example/demo/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.user.repository;
+
+import com.example.demo.user.entity.RefreshToken;
+import com.example.demo.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+    void deleteByUser(User user);
+
+}
