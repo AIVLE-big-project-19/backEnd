4c3875c8a5e8ad1c905bc3711a576ef20197870d
cxz2055-stack <cxz2055@gmail.com>
2026-07-14 10:07:32 +0900

    fix: Main Controller / Service분리

---
+++ src/main/java/com/example/demo/board/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.example.demo.board.controller;
+
+import com.example.demo.board.service.MainService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+public class MainController {
+
+    private final MainService mainService;
+
+
+    public MainController(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @GetMapping("/main") // 메인 화면 출력
+    public String showMap(Model model) {
+        model.addAttribute("vworldApiKey", mainService.getVWorldApiKey());
+
+
+        model.addAttribute("locations", getSampleLocations());
+
+        return "main";
+    }
+
+    @GetMapping("/api/map/search") //지도 검색
+
+    @ResponseBody
+    public String searchProxy(@RequestParam("keyword") String keyword) {
+        return mainService.searchVWorldPlace(keyword);
+    }
+
+    private List<Map<String, Object>> getSampleLocations() {
+        List<Map<String, Object>> sampleLocations = new ArrayList<>();
+        return sampleLocations;
+    }
+}
---
+++ src/main/java/com/example/demo/board/service/MainService.java
@@ -0,0 +1,37 @@
+package com.example.demo.board.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class MainService {
+
+    private final String vWorldApiKey = "0559A559-131E-35CB-90E4-93B1B10CA3E9"; //추후 properties나 evn로 빼기
+
+    public String searchVWorldPlace(String keyword) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            String encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8);
+            String urlStr = "https://api.vworld.kr/req/search?service=search&request=search&version=2.0"
+                    + "&crs=EPSG:900913"
+                    + "&size=10&page=1"
+                    + "&query=" + encodedKeyword
+                    + "&type=place"
+                    + "&format=json&errorformat=json"
+                    + "&key=" + vWorldApiKey;
+
+            URI uri = URI.create(urlStr);
+            return restTemplate.getForObject(uri, String.class);
+        } catch (Exception e) {
+            return "{\"response\":{\"status\":\"ERROR\",\"message\":\"" + e.getMessage() + "\"}}";
+        }
+    }
+
+    public String getVWorldApiKey() {
+        return this.vWorldApiKey;
+    }
+}
