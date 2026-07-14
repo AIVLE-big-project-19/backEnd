8bdb49282fb240b468ead7ee820e6517a682044f
s2ungyeon-h <s2ungyeon.h@gamil.com>
2026-07-14 09:21:34 +0900

    chore: AuthApiControllerTest 미사용 import 제거
    리뷰 지적사항(eq 미사용) 반영.

--- src/test/java/com/example/demo/user/controller/AuthApiControllerTest.java
+++ src/test/java/com/example/demo/user/controller/AuthApiControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
