e2858f5c048c28db1f71833aec6e8192262d5dab
s2ungyeon-h <s2ungyeon.h@gamil.com>
2026-07-16 10:55:54 +0900

    feat: 구글 로그인 에러/성공 코드 추가

--- src/main/java/com/example/demo/global/response/SuccessCode.java
+++ src/main/java/com/example/demo/global/response/SuccessCode.java
@@ -32,6 +32,7 @@ public enum SuccessCode {
     PASSWORD_CODE_VERIFIED("인증이 완료되었습니다."),
     PASSWORD_VERIFICATION_STATUS_CHECKED("인증 상태 조회 성공"),
     PASSWORD_RESET("비밀번호가 재설정되었습니다."),
+    GOOGLE_LOGIN("구글 로그인 성공"),
 
     // Chat
     CHAT_REPLIED("답변을 생성했습니다.");
